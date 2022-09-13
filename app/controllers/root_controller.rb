d13392fa236a9ccf4c10714dae5a7eb7b618805e
mar <marilirulita@gmail.com>
2022-09-13 11:26:10 -0700

    add message controller and rout

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,8 @@
 class RootController < ApplicationController
   def index
+    # @messages = Message.all
+    @message = Message.find(rand(1..Message.count))
+
+    render json: { greet: @message.text }, status: :ok
   end
 end
