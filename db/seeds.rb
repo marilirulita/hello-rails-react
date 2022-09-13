1b609f89ed3d9b20bd6deac745f5947a6e1eccb2
mar <marilirulita@gmail.com>
2022-09-13 11:07:59 -0700

    add message model and table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+messages = Message.create([{text: "How are things?
+  "}, { text: "What’s new?"}, { text: "What’s up?"}, { text: "What’s the story?"}, { text: "What’s happening?"}])
