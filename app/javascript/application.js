223b8ba15f1a396817e524fd5f31858015ebcb5b
mar <marilirulita@gmail.com>
2022-09-13 09:01:35 -0700

    add babel and react donfig

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './index.css';
+
+function App() {
+  return (<h1>Hi peoplegklhkl</h1>);
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route path="/" element={<App />} />
+  //   </Routes>
+  // </BrowserRouter>,
+);
